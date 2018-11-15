59e885c23183dcb306a358512d78b9c1c50cd356
Heather Miller <heather.miller114@gmail.com>
2018-11-15 00:40:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,6 +100,8 @@ class Student
       LIMIT 1
       SQL
       
-    DB[:conn].execute(sql).flatten
+    DB[:conn].execute(sql).flatten.map do |row|
+      self.new_from_db(row)
+    end
   end
 end
