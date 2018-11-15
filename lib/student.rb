4572b570ec0df30aa86050a99834f365c4d98020
Heather Miller <heather.miller114@gmail.com>
2018-11-15 00:46:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,8 +100,18 @@ class Student
       LIMIT 1
       SQL
       
-    DB[:conn].execute(sql).each do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
+  end
+  
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      SQL
+      
+    DB[:conn].execute(sql)
   end
 end
