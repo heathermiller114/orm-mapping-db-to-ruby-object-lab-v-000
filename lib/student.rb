c0c9b07e3a419d9be2e6ef4c2cf85b2df155ac48
Heather Miller <heather.miller114@gmail.com>
2018-11-15 00:39:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,16 +81,16 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10(number_of_students)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade = 10
-      LIMIT ?
-      SQL
-      
-    DB[:conn].execute(sql)
-  end
+  #def self.first_X_students_in_grade_10(number_of_students)
+  #  sql = <<-SQL
+  #    SELECT *
+  #    FROM students
+  #    WHERE grade = 10
+  #    LIMIT ?
+  #    SQL
+  #    
+  #  DB[:conn].execute(sql)
+  #end
   
   def self.first_student_in_grade_10
     sql = <<-SQL
@@ -100,6 +100,8 @@ class Student
       LIMIT 1
       SQL
       
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 end
